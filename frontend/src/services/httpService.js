104864c475cb4b1d678521e305f1b04d06f17072
Tejas Chaudhari <tejasgchaudhari@gmail.com>
2026-03-27 11:09:14 +0530

    fixed the httpservice to connect to backend

--- frontend/src/services/httpService.js
+++ frontend/src/services/httpService.js
@@ -5,19 +5,22 @@ const isLocalhost =
   typeof window !== "undefined" &&
   (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1");
 
-axios.defaults.baseURL = isLocalhost ? "http://localhost:10000/api" : "/api";
+
+const baseURL = isLocalhost
+  ? "http://localhost:10000/api"
+  : "https://vidly-backend-r69q.onrender.com/api"; // 
+
+axios.defaults.baseURL = baseURL;
 
 // RESPONSE INTERCEPTOR
 axios.interceptors.response.use(
   response => response,
   error => {
-    // Expected errors (4xx) → handled by calling code
     const expectedError =
       error.response &&
       error.response.status >= 400 &&
       error.response.status < 500;
 
-    // Unexpected errors (5xx / network)
     if (!expectedError) {
       toast.error("An unexpected error occurred.");
     }
@@ -35,7 +38,6 @@ function clearJwt() {
   delete axios.defaults.headers.common["x-auth-token"];
 }
 
-// Export HTTP methods
 export default {
   get: axios.get,
   post: axios.post,
